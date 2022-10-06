7d5b1654b899b9e49e4bea53fcdb7e7266baf0f7
Justin-Lavoie16 <113055699+Justin-Lavoie16@users.noreply.github.com>
2022-10-06 18:50:08 +0000

    dwadasad

--- js/script.js
+++ js/script.js
@@ -1,12 +1,5 @@
-function doMathClicked() {
-  document.getElementById("add-math").innerHTML =
-    "<p>6 + 5 = " + (6 + 5) + "</p>"
-  document.getElementById("subtract-math").innerHTML =
-    "<p>7 - 3 =  " + (7 - 3) + "</p>"
-  document.getElementById("multiply-math").innerHTML =
-    "<p>3 + 4 × 2 = " + (3 + 4 * 2) + "</p>"
-  document.getElementById("divide-math").innerHTML =
-    "<p>(4 ÷ 2) + 3 = " + (4 / 2 + 3) + "</p>"
-  document.getElementById("exponent-math").innerHTML =
-    "<p>5 + 2³ = " + (5 + 2 ** 3) + "</p>"
+function doMathClicked () {
+  
+  document.getElementById('area').innerHTML = 'The area is: ' + (5 * 3) + ' cm².'
+  document.getElementById('perimeter').innerHTML = 'The perimeter is: ' + (2 * (5 + 3)) + ' cm.'
 }
