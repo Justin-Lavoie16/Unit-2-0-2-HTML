de844de0a3a4ecb656f9d4974ce1f7f0fa882051
Justin-Lavoie16 <Justin-Lavoie16@users.noreply.github.com>
2022-10-06 18:52:09 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-function doMathClicked () {
-  
-  document.getElementById('area').innerHTML = 'The area is: ' + (5 * 3) + ' cm².'
-  document.getElementById('perimeter').innerHTML = 'The perimeter is: ' + (2 * (5 + 3)) + ' cm.'
+function doMathClicked() {
+  document.getElementById("area").innerHTML = "The area is: " + 5 * 3 + " cm²."
+  document.getElementById("perimeter").innerHTML =
+    "The perimeter is: " + 2 * (5 + 3) + " cm."
 }
